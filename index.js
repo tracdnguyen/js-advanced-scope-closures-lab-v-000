4de95e22d7dcc6dc520c05f6e7b3d17530aa157a
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-09 00:47:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function produceDrivingRange(distance) {
+  return function(starting, ending) {
+    const distanceTravelled = parseInt(ending) - parseInt(starting);
+
+    if (distanceTravelled > distance) {
+      return `${distanceTravelled - distance} blocks out of range`
+    } else {
+      return `within range by ${distance - distanceTravelled}`
+    };
+
+  };
+};
+
+
+function produceTipCalculator(percent) {
+  return function(tip) {
+    return (tip * percent);
+  };
+};
+
+
+function createDriver() {
+  let driverId = 0;
+
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+};
