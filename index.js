cc17880102ab07954da0fc2f89374decf88c0776
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-09 00:36:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function produceDrivingRange(distance) {
+  return function(starting, ending) {
+    const distanceTravelled = parseInt(ending) - parseInt(starting);
+
+    if distanceTravelled
+  };
+};
