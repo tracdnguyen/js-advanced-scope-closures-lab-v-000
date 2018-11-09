16ba5a78d36c00abaae755772f34362d1182b798
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-09 00:47:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,12 @@ function produceTipCalculator(percent) {
 
 
 function createDriver() {
-  
+  let driverId = 0;
+
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
 };
