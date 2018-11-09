1fbb802f62b29343f75384ddde7b668dd831fbbf
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-09 00:43:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function produceDrivingRange(distance) {
   return function(starting, ending) {
     const distanceTravelled = parseInt(ending) - parseInt(starting);
-    
+
     if (distanceTravelled > distance) {
       return `${distanceTravelled - distance} blocks out of range`
     } else {
       return `within range by ${distance - distanceTravelled}`
     };
+    
   };
 };
